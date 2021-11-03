49c7f382ec670931b40c3a1c5e290e9d5a262978
Chau Wing Wai <bbqaaq@gmail.com>
2021-11-03 16:18:16 +0800

    - add http server

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const rl = readline.createInterface({
 function ask() {
 	rl.question('Number 1? ', (n1) => {
 	    rl.question('Number 2? ', (n2) => {
-	        console.log(`result is ${n1 * n2}`)
+	        console.log(`Multiply result is ${n1 * n2}`)
 	        ask()
 	    })
 	})
@@ -16,3 +16,12 @@ function ask() {
 
 ask()
 
+const http = require('http')
+
+const requestListener = function (req, res) {
+  res.writeHead(200)
+  res.end('Hello, World!')
+}
+
+const server = http.createServer(requestListener)
+server.listen(3000)
